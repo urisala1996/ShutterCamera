2f8d9ea01505cc2eb55b731da2e4c769193ca314
urisala1996 <oriol0sala0sucarrats@gmail.com>
2017-09-29 21:12:54 +0200

    OpenCV Integration

--- app/src/main/java/pae/iot/shuttercamera/MainActivity.java
+++ app/src/main/java/pae/iot/shuttercamera/MainActivity.java
@@ -3,11 +3,20 @@ package pae.iot.shuttercamera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.opencv.android.OpenCVLoader;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Init OpenCV
+        if (BuildConfig.DEBUG) {
+            OpenCVLoader.initDebug();
+        }
+
+
     }
 }
